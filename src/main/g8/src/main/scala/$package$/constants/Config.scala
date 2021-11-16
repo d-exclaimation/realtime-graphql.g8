4f2f1d9aa214930544c03ac04026b8348e940ac5
d-exclaimation <70748917+d-exclaimation@users.noreply.github.com>
2021-11-16 14:41:56 +0700

    Update Config.scala

--- src/main/g8/src/main/scala/$package$/constants/Config.scala
+++ src/main/g8/src/main/scala/$package$/constants/Config.scala
@@ -11,6 +11,9 @@ import akka.http.scaladsl.model.headers.HttpOrigin
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
 import ch.megard.akka.http.cors.scaladsl.model.HttpOriginMatcher
 import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
+import $package$.constants.Environment.__prod__
+import $package$.services.Counter
+import sangria.execution.QueryReducer
 
 object Config {
   /** CORS Setup for allowing Apollo Sandbox */
@@ -18,4 +21,9 @@ object Config {
     .defaultSettings
     .withAllowedOrigins(HttpOriginMatcher(HttpOrigin("https://studio.apollographql.com")))
     .withAllowCredentials(true)
+  
+  /** Query Reducer based on RUNTIME_ENV */
+  val reducers: List[QueryReducer[Counter, _]] =
+    if (__prod__) QueryReducer.rejectIntrospection[Counter](includeTypeName = true) :: Nil
+    else Nil
 }
